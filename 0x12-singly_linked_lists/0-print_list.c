64d19e1542d90ff88db71c7fff6b969b205ae231
Pharlarposh <aderomolabolaji@gmail.com>
2022-08-14 20:54:32 -0700

    Tasks 0-4

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *print_list - entry point
+ *@h: arg one
+ *Return: number of nodes in linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
